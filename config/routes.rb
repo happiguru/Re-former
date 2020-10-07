d7b86fe930bd9e382d09a236aa8044c24f00008e
hguruman <hguruman@gmail.com>
2020-10-07 20:42:43 +0400

    Update user controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'users#create'
   resources :users, only: [:new, :create]
+
 end
